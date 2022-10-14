b3dd7bad156e2f270d6c3a72838a95296da21136
Josh Crane <josh.crane@mechanicalrock.io>
2022-10-14 21:27:04 +0800

    Removing old fixtures

--- fixtures/public/DATA.sql
+++
@@ -1,3 +0,0 @@
--- depends_on: {{ ref('TRANS01_DATA') }}
-{{ config(tags=['PROJECT', 'DATA']) }}
-{{  clone_table('TRANS01_DATA', "DATA") }}
--- fixtures/transform/TRANS01_DATA.sql
+++
@@ -1,7 +0,0 @@
-{{ config(tags=['PROJECT', 'DATA']) }}
-
-SELECT 
-    "V":id::string AS ID,
-    "V":name::string AS NAME,
-FROM 
-    {{ source('SOURCE_NAME', 'TABLE_NAME')}}
