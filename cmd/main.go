2d2e07fd8ee5433369953ea335c7a469cc169dae
Josh Crane <josh.crane@mechanicalrock.io>
2022-10-14 21:15:50 +0800

    Removing commented out code

--- cmd/main.go
+++ cmd/main.go
@@ -7,33 +7,3 @@ import (
 func main() {
 	templater.Main()
 }
-
-// func main() {
-// 	if len(os.Args) != 1 {
-// 		fmt.Fprintln(os.Stderr, "takes no arguments, run in the PROJECT folder and make sure CSV files are present")
-// 		os.Exit(1)
-// 	}
-
-// 	workingDir, err := os.Getwd()
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	var files []string
-// 	dir, _ := os.ReadDir(workingDir)
-// 	for _, file := range dir {
-// 		if strings.HasSuffix(file.Name(), ".csv") {
-// 			p := path.Join(workingDir, file.Name())
-// 			files = append(files, p)
-// 		}
-// 	}
-// 	if err != nil {
-// 		fmt.Fprintln(os.Stderr, err.Error())
-// 		os.Exit(1)
-// 	}
-
-// 	err = templater.GenerateTemplate(files)
-// 	if err != nil {
-// 		fmt.Fprintln(os.Stderr, err.Error())
-// 		os.Exit(1)
-// 	}
-// }
