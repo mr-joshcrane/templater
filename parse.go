d8126648d1d38032a1f8704430f0c2a8a881155d
Josh Crane <josh.crane@mechanicalrock.io>
2022-10-29 12:56:05 +0800

    Removing unneeded file:

--- parse.go
+++ parse.go
@@ -2,6 +2,7 @@ package templater
 
 import (
 	"errors"
+	"path/filepath"
 	"regexp"
 	"strings"
 
@@ -136,4 +137,31 @@ func stripAndEscapeQuotes(s string) string {
 	s = strings.ReplaceAll(s, `:`, `":"`)
 	s = strings.ReplaceAll(s, `.`, `"."`)
 	return s
+}
+
+var validCharacters = regexp.MustCompile(`[A-Z0-9._ ]*`)
+var camelCase = regexp.MustCompile(`([a-z])(A?)([A-Z])`)
+
+func NormaliseKey(s string) string {
+	s = camelCase.ReplaceAllString(s, `$1 $2 $3`)
+	s = strings.ToUpper(s)
+	s = strings.Join(validCharacters.FindAllString(s, -1), " ")
+	s = strings.Join(strings.Fields(s), "_")
+	s = strings.Trim(s, ` `)
+	s = strings.ReplaceAll(s, `.`, `__`)
+	s = strings.ReplaceAll(s, ` `, `_`)
+	return s
+}
+
+func cleanTableName(path string) string {
+	tableName := filepath.Base(path)
+	tableName = strings.ToUpper(tableName)
+	tableName = strings.ReplaceAll(tableName, ".CSV", "")
+	return tableName
+}
+
+type NameOption func(string) string
+
+func prefix(s string) string {
+	return "V:" + s
 }
