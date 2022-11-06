296474697cc8fd51b9df35133e145577c3e297dd
Josh Crane <josh.crane@mechanicalrock.io>
2022-11-06 13:12:29 +0800

    pulling create directories into own function

--- templater.go
+++ templater.go
@@ -26,19 +26,19 @@ type Table struct {
 	rawContents io.Reader
 }
 
-func ConvertToCueValue(c *cue.Context, r io.Reader) (cue.Value, error) {
+func ConvertTableData(c *cue.Context, r io.Reader) (cue.Iterator, error) {
 	buf := bytes.NewBuffer([]byte{})
 	df := dataframe.ReadCSV(r, dataframe.WithLazyQuotes(true))
 	err := df.WriteJSON(buf)
 	if err != nil {
-		return cue.Value{}, err
+		return cue.Iterator{}, err
 	}
-	return c.CompileBytes(buf.Bytes()), nil
+	cueValue := c.CompileBytes(buf.Bytes())
+	return cueValue.List()
+
 }
 
-func GenerateTemplateFiles(fsys fs.FS, projectName string, unpackPaths ...string) error {
-	c := cuecontext.New()
-
+func GenerateTables(fsys fs.FS, projectName string, unpackPaths ...string) ([]*Table, error) {
 	tables := []*Table{}
 	err := fs.WalkDir(fsys, ".", func(path string, info fs.DirEntry, err error) error {
 
@@ -63,20 +63,24 @@ func GenerateTemplateFiles(fsys fs.FS, projectName string, unpackPaths ...string
 		return nil
 	})
 	if err != nil {
+		return nil, err
+	}
+	return tables, nil
+}
+
+func GenerateTemplateFiles(fsys fs.FS, projectName string, unpackPaths ...string) error {
+	c := cuecontext.New()
+	tables, err := GenerateTables(fsys, projectName, unpackPaths...)
+	if err != nil {
 		return err
 	}
-
 	for _, table := range tables {
-		cueValue, err := ConvertToCueValue(c, table.rawContents)
+		iterator, err := ConvertTableData(c, table.rawContents)
 		if err != nil {
 			return err
 		}
 
-		iter, err := cueValue.List()
-		if err != nil {
-			return err
-		}
-		err = table.InferFields(iter, unpackPaths...)
+		err = table.InferFields(iterator, unpackPaths...)
 		if err != nil {
 			return err
 		}
@@ -106,6 +110,32 @@ func GenerateTemplateFiles(fsys fs.FS, projectName string, unpackPaths ...string
 	return WriteProperties(c, models, sources)
 }
 
+
+func createDirectories() error {
+	_, err := os.Stat("output")
+	if err != nil {
+		err := os.Mkdir("output", os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+	_, err = os.Stat("output/transform")
+	if err != nil {
+		err := os.Mkdir("output/transform", os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+	_, err = os.Stat("output/public")
+	if err != nil {
+		err := os.Mkdir("output/public", os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func Main() int {
 	workingDir, err := os.Getwd()
 	if err != nil {
@@ -114,29 +144,10 @@ func Main() int {
 
 	fsys := os.DirFS(workingDir)
 
-	_, err = os.Stat("output")
+	err = createDirectories()
 	if err != nil {
-		err := os.Mkdir("output", os.ModePerm)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err.Error())
-			return 1
-		}
-	}
-	_, err = os.Stat("output/transform")
-	if err != nil {
-		err := os.Mkdir("output/transform", os.ModePerm)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err.Error())
-			return 1
-		}
-	}
-	_, err = os.Stat("output/public")
-	if err != nil {
-		err := os.Mkdir("output/public", os.ModePerm)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err.Error())
-			return 1
-		}
+		fmt.Fprintln(os.Stderr, err.Error())
+		return 1
 	}
 
 	err = GenerateTemplateFiles(fsys, "PROJECT", os.Args[1:]...)
@@ -146,3 +157,4 @@ func Main() int {
 	}
 	return 0
 }
+
